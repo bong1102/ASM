82faa639a951f006bab5c7f7a4920c1e75fddae0
bong1102 <hoaltmgcd201723@fpt.edu.vn>
2023-06-06 19:37:43 +0700

    create react

---
+++ asm-25/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import Logo from "../assets/logo.jpg"
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className="leftSide">
+                <image alt="Logo" src="logo.jpg" />
+            </div>
+            <div className="rightSide"></div>
+        </div>
+    )
+}
+
+export default Navbar
